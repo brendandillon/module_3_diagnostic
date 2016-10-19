6f46915f2ffc643a14b6377cd91bdd044d1e4d55
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-10-19 09:32:34 -0600

    Add Capybara

--- spec/features/guest_locates_stations_spec.rb
+++ spec/features/guest_locates_stations_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.feature 'a guest searches by zip code' do
   scenario 'they see a list of nearby stations' do
+    first_station = {
+      name: 'UDR',
+      address: '800 Acoma St',
+      fuel_types: 'Electric',
+      distance: '0.31 miles',
+      access_times: '24 hours daily'
+    }
+
     visit '/'
     fill_in 'q', with: '80203'
     click_on 'Locate' 
@@ -10,6 +18,11 @@ RSpec.feature 'a guest searches by zip code' do
     expect(page).to have_content('10.')
     expect(page).not_to have_content('11.')
 
+    expect(page).to have_content(first_station[:name])
+    expect(page).to have_content(first_station[:address])
+    expect(page).to have_content(first_station[:fuel_types])
+    expect(page).to have_content(first_station[:distance])
+    expect(page).to have_content(first_station[:access_times])
   end
 end
 #As a user
